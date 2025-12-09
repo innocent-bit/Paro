fe604fff2e9bbe5498fd02ab5bb27872e0dc2ffc
innocent-bit <innocentxd022@gmail.com>
2025-12-09 09:18:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ load_dotenv()
 # Get this value from my.telegram.org/apps
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-API_KEY = "xbit_GEWQY8QICOM0RGZKKPZTM2"
+API_KEY = "30DxNexGenBots6ac4cd"
 
 
 # Get your token from @BotFather on Telegram.
